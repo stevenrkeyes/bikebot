7f046741707d807adf2a288d009e15bdb516e51b
rfong <rachefo@gmail.com>
2015-09-02 19:58:49 -0700

    dump scraped data to sqlite db

--- models.py
+++ models.py
@@ -29,13 +29,14 @@ class Component(Model):
   name = CharField()
   price = FloatField()  # USD
   specs = JsonField()  # dictionary of specs
+  source = CharField()  # source of info
+
+
+# TODO: this should be somewhere safer
+db.connect()
+Component.create_table(True)
 
 
 if __name__ == '__main__':
-  db.connect()
-  Component.create_table(True)
-
-  Component.create(name="fancy-ass wheelset", price=1099.99, specs={u'tire size': u'700C', u'rating': u'4.5', u'weight': u'1570g', u'lacing pattern': u'Front: Radial; Rear: Radial/2-Cross', u'accessories included': u'2 pair of Cryo Blue Brake Pads', u'hub': u'Reynolds Racing', u'skewers included': u'Yes', u'spacing': u'Front: 100mm; Rear: 130mm', u'rim width': u'20.8mm', u'spoke count': u'Front: 20; Rear: 24', u'rim height': u'46mm', u'video': u'WALJK8Wh7I0', u'spokes': u'Bladed', u'compatibility': u'Shimano/SRAM 9/10/11-speed', u'tire type': u'Clincher'})
-
-  component = Component.select().where(Component.name == 'fancy-ass wheelset')[0]
-  print component.name, component.price
+  for comp in Component.select():
+    print comp.name, comp.price
---
+++ performancebike.py
@@ -0,0 +1,66 @@
+# tmp shitty scraper for performancebike.com until I figure out how to kimono
+
+from bs4 import BeautifulSoup
+import os
+import urllib2
+
+from models import Component
+
+
+def clean(s):
+  return s.strip().strip(':')
+
+def parse_currency(s):
+  return float(s.strip('$').replace(',', ''))
+
+
+class PageParser(object):
+
+  source = 'performancebike'
+
+  def __init__(self, url):
+    self.page = BeautifulSoup(urllib2.urlopen(url))
+
+  def parse_page(self):
+    return {
+      'name': self.get_first_by_selector('.product_title').strip(),
+      'specs': dict(self.parse_table(
+        ['#specsDiv > dl > dt', '#specsDiv > dl > dd'])),
+      'price': parse_currency(
+        self.get_first_by_selector('.sr_product_price .sale_price_val') or
+        self.get_by_selector('.sr_product_price .list_price_val')),
+      'source': self.source,
+    }
+  
+  def get_by_selector(self, selector):
+    '''Return all tag contents matching selector'''
+    return [clean(tag.string) for tag in self.page.select(selector) if tag]
+
+  def get_first_by_selector(self, selector):
+    '''Return content of first tag matching selector'''
+    tags = self.page.select(selector)
+    if not tags:
+      return None
+    return tags[0].string
+
+  def parse_table(self, selectors):
+    '''
+    Given list of selectors, return list of tuples containing table row data
+    '''
+    return zip(*(self.get_by_selector(selector) for selector in selectors))
+
+
+if __name__ == '__main__':
+  urls = [
+    # component page
+    'http://www.performancebike.com/bikes/Product_10052_10551_1175974_-1_400222__400222',
+    # full bike page
+    'http://www.performancebike.com/bikes/Product_10052_10551_1161804_-1_400317__400317',
+  ]
+
+  # Scrape pages and dump data into sqlite db
+  for url in urls:
+    comp = Component.create(**PageParser(url).parse_page())
+
+  for comp in Component.select():
+    print '%s (%s) - %s' % (comp.name, comp.price, comp.source)
