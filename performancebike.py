8a4b2e90334f3fd26008fa9b801726153410dc06
rfong <rachefo@gmail.com>
2015-09-03 21:41:48 -0700

    get category paginated urls

--- performancebike.py
+++ performancebike.py
@@ -1,7 +1,10 @@
 # tmp shitty scraper for performancebike.com until I figure out how to kimono
 
 from bs4 import BeautifulSoup
+import math
 import os
+import re
+import urllib
 import urllib2
 
 from models import Component
@@ -12,6 +15,38 @@ def clean(s):
 
 def parse_currency(s):
   return float(s.strip('$').replace(',', ''))
+
+
+PAGE_SIZE = 12
+#http://www.performancebike.com/bikes/SubCategory_10052_10551_400219_-1_400002_400038?beginIndex=12&pageSize=12
+
+def get_product_count(category_url):
+  '''Given category page, get total product count'''
+  # TODO: make this not terrible / hardcoded to DOM assumptions
+  page = PageParser(category_url)
+  s = page.get_first_by_selector('.item-numbers').strip()
+  match = re.search(re.compile('[0-9]+$'), s)
+  if match:
+    return int(match.group(0))
+  return None
+
+def get_paginated_urls(category_url):
+  '''Given first page's URL (no params), return all available paginated URLs'''
+  num_products = get_product_count(category_url)
+  num_pages = int(math.ceil(num_products / PAGE_SIZE))
+
+  urls = [category_url]
+  if num_pages > 1:
+    for p in xrange(1, num_pages + 1):
+      urls.append(
+        category_url + '?' +
+        urllib.urlencode({'pageSize': PAGE_SIZE, 'beginIndex': p * PAGE_SIZE})
+      )
+  return urls
+
+def get_product_urls(category_url):
+  '''Given category first page URL (no params), return all product URLs'''
+  pass
 
 
 class PageParser(object):
@@ -59,8 +94,11 @@ if __name__ == '__main__':
   ]
 
   # Scrape pages and dump data into sqlite db
-  for url in urls:
-    comp = Component.create(**PageParser(url).parse_page())
+  #for url in urls:
+  #  comp = Component.create(**PageParser(url).parse_page())
 
   for comp in Component.select():
     print '%s (%s) - %s' % (comp.name, comp.price, comp.source)
+
+  print get_paginated_urls('http://www.performancebike.com/bikes/SubCategory_10052_10551_400219_-1_400002_400038')
+
